1557de6c3e620499a281ca18038004b0b65dabca
Samuel English <sdenglis@ucsc.edu>
2019-04-13 11:40:25 -0700

    Finished all functions except for extra credit.

--- Lab02/CE13Lab2.X/Calculator.c
+++ Lab02/CE13Lab2.X/Calculator.c
@@ -39,6 +39,7 @@ void CalculatorRun(void)
 
     printf("Please choose an operation (*, /, +, -, m, a, c, f, t, r): ");
     scanf("%c", &operation);
+    while (1);
     printf("You input '%c'", operation);
 
     /*
@@ -64,74 +65,80 @@ double Multiply(double operand1, double operand2)
 double Divide(double operand1, double operand2)
 {
     //  Your code here
-    double quotient = (operand1 / operand2);
-    return quotient;
-}
-
-double Add(double operand1, double operand2)
-{
-    //  Your code here
-    double sum = (operand1 + operand2);
-    return sum;
-}
-
-double Subtract(double operand1, double operand2)
-{
-    //  Your code here
-    double difference = (operand1 - operand2);
-    return difference;
-}
-
-double Average(double operand1, double operand2)
-{
-    //  Your code here
-    double mean = ((operand1 + operand2) / 2)
-    return mean;
-}
-
-double AbsoluteValue(double operand)
-{
-    //  Your code here
-    if (operand < 0) {
-        double positive = (operand * (-1))
+    if (operand2 = 0) {
+        // Add something here.
+        return 0;
     } else {
-        double positive = (operand)
+        double quotient = (operand1 / operand2);
+        return quotient;
     }
-    
-    return positive;
+
+    double Add(double operand1, double operand2)
+    {
+        //  Your code here
+        double sum = (operand1 + operand2);
+        return sum;
+    }
+
+    double Subtract(double operand1, double operand2)
+    {
+        //  Your code here
+        double difference = (operand1 - operand2);
+        return difference;
+    }
+
+    double Average(double operand1, double operand2)
+    {
+        //  Your code here
+        double mean = ((operand1 + operand2) / 2.0f);
+        return mean;
+    }
+
+    double AbsoluteValue(double operand)
+    {
+        //  Your code here
+        if (operand < 0) {
+            double positive = (operand * (-1));
+            return positive;
+        } else {
+            double positive = (operand);
+            return positive;
+        }
+    }
+
+    double FahrenheitToCelsius(double operand)
+    {
+        //  Your code here
+        double F2C = ((operand - 32) * (5.0 / 9.0f));
+        return F2C;
+    }
+
+    double CelsiusToFahrenheit(double operand)
+    {
+        //  Your code here
+        double C2F = ((operand * (9.0 / 5.0f)) + 32);
+        return C2F;
+    }
+
+    double Tangent(double operand)
+    {
+        //  Converts degree input into radians
+        double tan = tan((operand * M_PI) / 180.0f);
+        return tan;
+    }
 }
 
-double FahrenheitToCelsius(double operand)
-{
-    //  Your code here
-    double F2C =
-    return 0;
-}
-
-double CelsiusToFahrenheit(double operand)
-{
-    //  Your code here
-    double C2F =
-    return 0;
-}
-
-double Tangent(double operand)
-{
-    //  Your code here
-    double tan =
-    return 0;
-}
 
 /*********************************************************************************
  * Define the Round function here.
  * In order to receive the extra credit your calculator MUST ALSO CORRECTLY utilize
  * this function.
- ********************************************************************************/
+ *******************************************************************************
 double Round(double operand)
 {
     //  Your code here
     double ceiling =
     return 0;
 }
-
+ */
 
