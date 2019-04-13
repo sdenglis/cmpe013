975ee16fec5677d7e8a413a16c0e9a83dbd6456d
Samuel English <sdenglis@ucsc.edu>
2019-04-13 11:00:06 -0700

    Test commit from inside MPLAB X IDE. Many of the functions are now complete!

--- Lab02/CE13Lab2.X/Calculator.c
+++ Lab02/CE13Lab2.X/Calculator.c
@@ -20,9 +20,9 @@ double Multiply(double operand1, double operand2);
 double Divide(double operand1, double operand2);
 double Add(double operand1, double operand2);
 double Subtract(double operand1, double operand2);
+double Average(double operand1, double operand2);
 
 // Single-input function prototypes
-double Average(double operand);
 double AbsoluteValue(double operand);
 double FahrenheitToCelsius(double operand);
 double CelsiusToFahrenheit(double operand);
@@ -35,16 +35,21 @@ void CalculatorRun(void)
 
     // Your code here
 
-    printf("Please choose an operation (*, /, +, -, m, a, c, f, t, r): ");
-    scanf();
+    char operation;
 
+    printf("Please choose an operation (*, /, +, -, m, a, c, f, t, r): ");
+    scanf("%c", &operation);
+    printf("You input '%c'", operation);
+
+    /*
     printf("Enter first operand: ");
     scanf();
     printf("Enter second operand: ");
     scanf();
-    
+
     printf("Enter operand: ");
     scanf();
+     */
 
     while (1);
 }
@@ -52,46 +57,69 @@ void CalculatorRun(void)
 double Multiply(double operand1, double operand2)
 {
     //  Your code here
+    double product = (operand1 * operand2);
+    return product;
 }
 
 double Divide(double operand1, double operand2)
 {
     //  Your code here
+    double quotient = (operand1 / operand2);
+    return quotient;
 }
 
 double Add(double operand1, double operand2)
 {
     //  Your code here
+    double sum = (operand1 + operand2);
+    return sum;
 }
 
 double Subtract(double operand1, double operand2)
 {
     //  Your code here
+    double difference = (operand1 - operand2);
+    return difference;
 }
 
 double Average(double operand1, double operand2)
 {
     //  Your code here
+    double mean = ((operand1 + operand2) / 2)
+    return mean;
 }
 
 double AbsoluteValue(double operand)
 {
     //  Your code here
+    if (operand < 0) {
+        double positive = (operand * (-1))
+    } else {
+        double positive = (operand)
+    }
+    
+    return positive;
 }
 
 double FahrenheitToCelsius(double operand)
 {
     //  Your code here
+    double F2C =
+    return 0;
 }
 
 double CelsiusToFahrenheit(double operand)
 {
     //  Your code here
+    double C2F =
+    return 0;
 }
 
 double Tangent(double operand)
 {
     //  Your code here
+    double tan =
+    return 0;
 }
 
 /*********************************************************************************
@@ -102,6 +130,8 @@ double Tangent(double operand)
 double Round(double operand)
 {
     //  Your code here
+    double ceiling =
+    return 0;
 }
 
 
