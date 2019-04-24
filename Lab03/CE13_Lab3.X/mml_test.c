cb8f26bfef6efa9036853ce29836313aeb241b22
Samuel English <sdenglis@ucsc.edu>
2019-04-24 09:18:07 -0700

    Fixed more stuff, cleaning things up!

--- Lab03/CE13_Lab3.X/mml_test.c
+++ Lab03/CE13_Lab3.X/mml_test.c
@@ -15,135 +15,35 @@
 #include "MatrixMath.h"
 
 // Module-level variables:
-float x = 3.0;
-float trace = 0;
-float zero_matrix[3][3] = {};
 float result[3][3] = {};
 float Result[2][2] = {};
-float mat [3][3] = {
-    {1, -2, 3},
-    {1, 2, -3},
-    {-1, 2, 3}
-};
-float mat1[3][3] = {
-    {1, -2, 3},
-    {1, 2, -3},
-    {-1, 2, 3}
-};
-float mat2[3][3] = {
-    {1, -2, 3},
-    {1, 2, -3},
-    {-1, 2, 3}
-};
+
+
 
 int main()
 {
     BOARD_Init();
 
-    printf("Beginning sdenglis's mml test harness, compiled on %s %s\n", __DATE__, __TIME__);
+    printf("Beginning sdenglis's mml test harness, compiled on %s %s\n\n", __DATE__, __TIME__);
 
-    printf("Demonstrating MatrixPrint():\n");
-    {
-        // Print out first matrix
-        MatrixPrint(mat1);
-        printf("\n");
-        // Print out second matrix
-        MatrixPrint(mat2);
 
-        // Print condition runs through MatrixEquals function
-        printf("Demonstrating MatrixEquals():\n");
-        {
-            if (MatrixEquals(mat1, mat2) == 0) {
-                printf("The two matrices are equal!\n");
-            }
-            if (MatrixEquals(mat1, mat2) == 1) {
-                printf("The two matrices are NOT equal.\n");
-            }
-        }
-    }
-
-    printf("\n");
-
-    printf("Demonstrating MatrixPrint():\n");
-    {
-
-        MatrixPrint(zero_matrix);
-        printf("\n");
-        MatrixPrint(mat2);
-
-        // Print condition runs through MatrixEquals function
-        printf("Demonstrating MatrixEquals():\n");
-        {
-            if (MatrixEquals(zero_matrix, mat2) == 1) {
-                printf("The two matrices are equal!\n");
-            }
-            if (MatrixEquals(zero_matrix, mat2) == 0) {
-                printf("The two matrices are NOT equal.\n");
-            }
-        }
-
-    }
-
-    printf("\n");
-
-    printf("Demonstrating MatrixPrint():\n");
-    {
-        // Print out first matrix
-        MatrixPrint(mat1);
-        printf("\n");
-        // Print out second matrix
-        MatrixPrint(mat2);
-        printf("\nDemonstrating MatrixAdd():\n");
-        {
-            MatrixAdd(mat1, mat2, result);
-            MatrixPrint(result);
-        }
-        printf("\nDemonstrating MatrixMultiply():\n");
-        {
-            // float result[3][3] = {};
-            MatrixMultiply(mat1, mat2, result);
-            // Function not correctly implemented
-            MatrixPrint(result);
-        }
-        printf("\nDemonstrating MatrixScalarAdd():\n");
-        {
-            // float result[3][3] = {};
-            MatrixScalarAdd(x, mat, result);
-            MatrixPrint(result);
-        }
-        printf("\nDemonstrating MatrixScalarMultiply():\n");
-        {
-            // float result[3][3] = {};
-            MatrixScalarMultiply(x, mat, result);
-            MatrixPrint(result);
-        }
-        printf("\nDemonstrating MatrixTrace():\n");
-        {
-            // float result[3][3] = {};
-            printf("Trace value: %f\n", MatrixTrace(mat));
-        }
-        printf("\nDemonstrating MatrixTranspose():\n");
-        {
-            MatrixTranspose(mat, result);
-            MatrixPrint(result);
-        }
-        float result[3][3] = {};
-        printf("\nDemonstrating MatrixDeterminant():\n");
-        {
-            printf("Determinant value: %f\n", MatrixDeterminant(mat));
-        }
-        printf("\nDemonstrating MatrixInverse():\n");
-        {
-            MatrixInverse(mat, result);
-            MatrixPrint(result);
-        }
-    }
-
-    printf("\n\n\n");
 
     /******************************************************************************
      * MATRIX - Matrix Equals
      *****************************************************************************/
+
+    float zero_matrix[3][3] = {};
+    float mat1[3][3] = {
+        {1, -2, 3},
+        {1, 2, -3},
+        {-1, 2, 3}
+    };
+    float mat2[3][3] = {
+        {1, -2, 3},
+        {1, 2, -3},
+        {-1, 2, 3}
+    };
+
 
     int MatrixEqualsTest = 0;
     // These two matrices should be equal!
@@ -246,7 +146,7 @@ int main()
     int MatrixDeterminantTest = 0;
     float knownDeterminant = 24;
     float anotherKnownDeterminant = 763.7;
-    
+
     float knownMatrix[3][3] = {
         {1, -2, 3},
         {1, 2, -3},
@@ -257,13 +157,13 @@ int main()
         {4, 6.2, -3},
         {-4, 7.1, 9}
     };
-    
+
     float experimentalDeterminant = MatrixDeterminant(knownMatrix);
     // Calculate the determinant of knownMatrix and compare to hard-coded value
     if (ScalarEquals(experimentalDeterminant, knownDeterminant) == 1) {
         MatrixDeterminantTest += 1;
     }
-    
+
     float anotherExperimentalDeterminant = MatrixDeterminant(anotherKnownMatrix);
     // Calculate the determinant of anotherKnownMatrix and compare to hard-coded value
     if (ScalarEquals(anotherExperimentalDeterminant, anotherKnownDeterminant) == 1) {
